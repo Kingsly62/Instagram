4217b7e9b17b54b06a1c491001e177e5c5abb3bd
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-03-04 12:54:09 +0300

    add authentication and cards in the adventure template file

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,8 +6,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('contact', views.contact, name='contact'),
     path('adventure', views.adventure, name='adventure'),
-    path('register', views.register, name='register'),
+    path('register', views.register_view, name='register'),
     # path('logout', views.logout, name='logout')
-    path('login', views.login, name='login'),
+    path('login', views.login_view, name='login'),
     path('ocean', views.ocean, name='ocean')
 ]
--- instagram/views.py
+++ instagram/views.py
@@ -1,19 +1,27 @@
 
 from django.urls import path
 from django.shortcuts import redirect, render
-from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
-from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
-from .forms import CreateUserForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
+from .models import *
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import (
+    authenticate,
+    get_user_model,
+    login,
+    logout
+)
+
+from .forms import UserLoginForm, UserRegisterForm
+
+
 # Create your views here.
 
-
+@login_required
 def home(request):
     return render(request, 'home.html')
 
@@ -27,20 +35,43 @@ def adventure(request):
     return render(request, 'adventure.html')
 
 
-def register(request):
+def register_view(request):
+    next = request.GET.get('next')
+    form = UserRegisterForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        password = form.cleaned_data.get('password')
+        user.set_password(password)
+        user.save()
+        new_user = authenticate(username=user.username, password=password)
+        login(request, new_user)
+        if next:
+            return redirect(next)
+        return redirect('adventure')
 
-    return render(request, 'register.html')
+    context = {
+        'form': form,
+    }
+    return render(request, "register.html", context)
+
+
+def login_view(request):
+    next = request.GET.get('next')
+    form = UserLoginForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        if next:
+            return redirect(next)
+        return redirect('adventure')
+
+    context = {
+        'form': form,
+    }
+    return render(request, "login.html", context)
 
 
 def ocean(request):
     return render(request, 'ocean.html')
-
-
-@login_required
-def login(reqiured):
-    return render(reqiured, 'login.html')
-
-
-@login_required
-def profile(request):
-    return render(request, "profile.html")
