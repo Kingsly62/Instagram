6e6eb238ab5db4e993bdb497b34d856d712908bf
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-03-07 19:45:10 +0300

    add new deployment

--- instaphoto/settings.py
+++ instaphoto/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$(iy1_axpugl2gb+$x8&*mw031&x0pb$ks7rcxvr=b^)6ety*&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
