08b15d8bb193808921193a71ca2f4a51640a3cd9
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-03-07 19:54:54 +0300

    add new deployment

--- instaphoto/settings.py
+++ instaphoto/settings.py
@@ -24,10 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$(iy1_axpugl2gb+$x8&*mw031&x0pb$ks7rcxvr=b^)6ety*&'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+MODE = config("MODE", default='dev')
+SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = []
 
