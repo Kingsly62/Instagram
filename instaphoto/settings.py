e146cc3bca2c7988496666989ecea3a15941bf2f
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-03-07 22:33:47 +0300

    add new deployment

--- instaphoto/settings.py
+++ instaphoto/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_URL = '/images/'
+
 STATICFILES = (os.path.join(BASE_DIR, 'static'))
 STATICFILES_DIRS = [
     'instaphoto/static'
