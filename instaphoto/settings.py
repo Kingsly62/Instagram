5142cfec8b41eebe7838186205428d0a9680f86e
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-03-07 19:40:51 +0300

    add new deployment

--- instaphoto/settings.py
+++ instaphoto/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
 ]
 django_heroku.settings(locals())
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
