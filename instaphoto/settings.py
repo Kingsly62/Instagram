3770a3521bbe5eb2793e9aa0dec5d72068de5c76
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-03-08 13:25:57 +0300

    add new updates

--- instaphoto/settings.py
+++ instaphoto/settings.py
@@ -91,8 +91,12 @@ WSGI_APPLICATION = 'instaphoto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': ''
     }
 }
 
@@ -153,3 +157,6 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS')
 EMAIL_PORT = config('EMAIL_PORT')
+
+
+# heroku pg:push instagram DATABASE_URL --app instagram-clinton
