0e4c21639d0986681b0e19a0f855b0f398163362
Kingsly62 <clinton.makori@student.moringaschool.com>
2022-03-11 08:42:43 +0300

    add new changes

--- instagram/urls.py
+++ instagram/urls.py
@@ -6,13 +6,13 @@ from .views import send_gmail
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('contact', views.contact, name='contact'),
-    path('adventure', views.adventure, name='adventure'),
-    path('register', views.register_view, name='register'),
+    path('contact/', views.contact, name='contact'),
+    path('adventure/', views.adventure, name='adventure'),
+    path('register/', views.register_view, name='register'),
     # path('logout', views.logout, name='logout')
-    path('login', views.login_view, name='login'),
+    path('login/', views.login_view, name='login'),
     path('ocean', views.ocean, name='ocean'),
     path('send_mail/', send_gmail, name="send_mail"),
-    path('welcome', views.welcome, name='welcome')
+    path('welcome/', views.welcome, name='welcome')
 
 ]
